3e7267840c887837248715067f351be90df44bec
sridharbandi <sridhar.bandi.ece@gmail.com>
2018-06-12 22:16:33 +0100

    Updated to New Version

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -2,12 +2,22 @@ import React, {Component} from 'react';
 import Aux from '../../hoc/Aux';
 import Header from '../Header/Header';
 
+
 class Board extends Component {
+
+    board = () =>
+        Array.from({length: 3},
+            () => Array.from({length: 3},
+                () => 0
+            )
+        );
 
     render() {
         return (
             <Aux>
                 <Header/>
+
+
             </Aux>
         )
     }
