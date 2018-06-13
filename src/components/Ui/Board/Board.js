1cadc5b38f0ded2a07d6524242a3d38254dddaf8
sridharbandi <sridhar.bandi.ece@gmail.com>
2018-06-13 13:45:56 +0100

    Ripple touch for Paper

--- src/components/Ui/Board/Board.js
+++ src/components/Ui/Board/Board.js
@@ -3,6 +3,10 @@ import Aux from '../../../hoc/Aux';
 import Grid from "@material-ui/core/Grid";
 import Paper from "@material-ui/core/Paper";
 import './Board.css';
+import ButtonBase from "@material-ui/core/ButtonBase";
+import Circle from '@material-ui/icons/FiberManualRecord';
+import Cross from '@material-ui/icons/NotInterested';
+import Icon from '@material-ui/core/Icon';
 
 
 class Board extends Component {
@@ -16,9 +20,15 @@ class Board extends Component {
 
     render() {
         const boardgui = this.board.map((row, rowId) => {
-            const columns = row.map((value, i) => (
-                <Grid key={i} item >
-                    <Paper elevation={4} className="Paper"/>
+            const columns = row.map((column, columnId) => (
+                <Grid key={columnId} item>
+                    <ButtonBase>
+                        <Paper elevation={4} className="Paper">
+                            <Icon color="action"  style={{ fontSize: 78 }}>
+                                fiber_manual_record
+                            </Icon>
+                        </Paper>
+                    </ButtonBase>
                 </Grid>
             ));
             return <Grid key={rowId} className="Grid" container justify="center" spacing={16}>
