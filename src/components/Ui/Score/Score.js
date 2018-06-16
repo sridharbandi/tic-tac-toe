86a86656022eb31825294ee44b841c7d2ffbbd12
sridharbandi <sridhar.bandi.ece@gmail.com>
2018-06-17 00:17:47 +0100

    Styles Scorecard

--- src/components/Ui/Score/Score.js
+++ src/components/Ui/Score/Score.js
@@ -2,10 +2,68 @@ import React from 'react';
 import Snackbar from '@material-ui/core/Snackbar';
 import IconButton from '@material-ui/core/IconButton';
 import CloseIcon from '@material-ui/icons/Close';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
+import Aux from '../../../hoc/Aux';
+import "./Score.css"
 
 const scorecard = (props) => {
 
-    const {open, onClose} = props
+    const {open, onClose} = props;
+
+    const action = (
+        <IconButton
+            key="close"
+            aria-label="Close"
+            color="inherit"
+            onClick={onClose}
+        >
+            <CloseIcon/>
+        </IconButton>
+    );
+
+    const score = (
+        <Aux>
+            <Divider className="Divider"/>
+            <Grid container spacing={16}>
+                <Grid item xs={4} >
+                    <Typography variant="body2" color="secondary">
+                        YourWins
+                    </Typography>
+                </Grid>
+                <Grid item xs={4} >
+                    <Typography variant="body2" color="secondary">
+                        Draws
+                    </Typography>
+                </Grid>
+                <Grid item xs={4} >
+                    <Typography variant="body2" color="secondary">
+                        OppWins
+                    </Typography>
+                </Grid>
+            </Grid>
+            <Divider className="Divider"/>
+            <Grid container style={{marginTop:'5px'}}>
+                <Grid item xs={4}>
+                    <Typography variant="caption" color="secondary" className="Scores">
+                        2
+                    </Typography>
+                </Grid>
+                <Grid item xs={4}>
+                    <Typography variant="caption" color="secondary" className="Scores">
+                        3
+                    </Typography>
+                </Grid>
+                <Grid item xs={4}>
+                    <Typography variant="caption" color="secondary" className="Scores">
+                        4
+                    </Typography>
+                </Grid>
+            </Grid>
+
+        </Aux>
+    );
 
     return (
         <Snackbar
@@ -17,17 +75,8 @@ const scorecard = (props) => {
             ContentProps={{
                 'aria-describedby': 'message-id',
             }}
-            message={<span id="message-id">Score here</span>}
-            action={[
-                <IconButton
-                    key="close"
-                    aria-label="Close"
-                    color="inherit"
-                    onClick={onClose}
-                    >
-                    <CloseIcon/>
-                </IconButton>,
-            ]}
+            message={score}
+            action={action}
         />
     );
 }
